88e5eaf942a83358ebf35dd6639f1b3be226dc9a
Welington Fidelis <welingtonfidelis@gmail.com>
2025-08-08 10:35:34 -0300

    Show favorite icon and add/remove recipe action included on recipes page (need some fixes);

--- lib/ui/recipes/recipes_view.dart
+++ lib/ui/recipes/recipes_view.dart
@@ -1,4 +1,5 @@
 import 'package:app4_receitas/di/service_locator.dart';
+import 'package:app4_receitas/ui/fav_recipes/fav_recipes_view_model.dart';
 import 'package:app4_receitas/ui/recipes/recipes_view_model.dart';
 import 'package:app4_receitas/ui/widgets/recipe_card.dart';
 import 'package:flutter/material.dart';
@@ -14,12 +15,14 @@ class RecipesView extends StatefulWidget {
 
 class _RecipesViewState extends State<RecipesView> {
   final viewModel = getIt<RecipesViewModel>();
+  final favRecipeViewModel = getIt<FavRecipesViewModel>();
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       viewModel.getRecipes();
+      favRecipeViewModel.getFavRecipes();
     });
   }
 
@@ -64,8 +67,7 @@ class _RecipesViewState extends State<RecipesView> {
         child: Column(
           children: [
             Expanded(
-              child: 
-              viewModel.recipes.isNotEmpty
+              child: viewModel.recipes.isNotEmpty
                   ? Center(
                       child: Column(
                         children: [
@@ -82,9 +84,13 @@ class _RecipesViewState extends State<RecipesView> {
                               itemCount: viewModel.recipes.length,
                               itemBuilder: (context, index) {
                                 final recipe = viewModel.recipes[index];
-                                // final isFavorite = viewModel.favRecipes.any(
-                                //   (fav) => fav.id == recipe.id,
-                                // );
+
+                                // TODO fix dont update icon after add/remove fav
+                                final isFavorite = favRecipeViewModel
+                                    .isFavorite(recipe.id);
+                                final toggleFavErrorMessage =
+                                    favRecipeViewModel.toggleFavErrorMessage;
+
                                 return Stack(
                                   children: [
                                     GestureDetector(
@@ -92,70 +98,127 @@ class _RecipesViewState extends State<RecipesView> {
                                           context.go('/recipe/${recipe.id}'),
                                       child: RecipeCard(recipe: recipe),
                                     ),
-                                    // Positioned(
-                                    //   top: 16,
-                                    //   right: 16,
-                                    //   child: IconButton(
-                                    //     icon: Icon(
-                                    //       isFavorite
-                                    //           ? Icons.favorite
-                                    //           : Icons.favorite_border,
-                                    //       size: 32,
-                                    //       color: isFavorite ? Colors.red : null,
-                                    //     ),
-                                    //     onPressed: () {
-                                    //       if (isFavorite) {
-                                    //         viewModel.removeFromFavorites(
-                                    //           recipe,
-                                    //         );
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} desfavoritada!',
-                                    //               ),
-                                    //               duration: Duration(
-                                    //                 seconds: 3,
-                                    //               ),
-                                    //               action: SnackBarAction(
-                                    //                 label: 'DESFAZER',
-                                    //                 onPressed: () {
-                                    //                   viewModel.addToFavorites(
-                                    //                     recipe,
-                                    //                   );
-                                    //                 },
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       } else {
-                                    //         viewModel.addToFavorites(recipe);
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} favoritada!',
-                                    //               ),
-                                    //               duration: const Duration(
-                                    //                 seconds: 2,
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       }
-                                    //     },
-                                    //   ),
-                                    // ),
+                                    Positioned(
+                                      top: 16,
+                                      right: 8,
+                                      child: IconButton(
+                                        icon: Icon(
+                                          isFavorite
+                                              ? Icons.favorite
+                                              : Icons.favorite_border,
+                                          size: 32,
+                                          color: isFavorite ? Colors.red : null,
+                                        ),
+                                        onPressed: () {
+                                          if (isFavorite) {
+                                            favRecipeViewModel.removeFavRecipe(
+                                              recipe.id,
+                                            );
+
+                                            // TODO fix dont showing error message
+                                            if (toggleFavErrorMessage != '') {
+                                              ScaffoldMessenger.of(
+                                                context,
+                                              ).clearSnackBars();
+                                              ScaffoldMessenger.of(
+                                                context,
+                                              ).showSnackBar(
+                                                SnackBar(
+                                                  content: Text(
+                                                    toggleFavErrorMessage!,
+                                                  ),
+                                                  duration: Duration(
+                                                    seconds: 6,
+                                                  ),
+                                                  // action: SnackBarAction(
+                                                  //   label: 'DESFAZER',
+                                                  //   onPressed: () {
+                                                  //     viewModel.addToFavorites(
+                                                  //       recipe,
+                                                  //     );
+                                                  //   },
+                                                  // ),
+                                                ),
+                                              );
+                                            }
+
+                                            // if (context.mounted) {
+                                            //   ScaffoldMessenger.of(
+                                            //     context,
+                                            //   ).clearSnackBars();
+                                            //   ScaffoldMessenger.of(
+                                            //     context,
+                                            //   ).showSnackBar(
+                                            //     SnackBar(
+                                            //       content: Text(
+                                            //         '${recipe.name} desfavoritada!',
+                                            //       ),
+                                            //       duration: Duration(
+                                            //         seconds: 3,
+                                            //       ),
+                                            //       action: SnackBarAction(
+                                            //         label: 'DESFAZER',
+                                            //         onPressed: () {
+                                            //           viewModel.addToFavorites(
+                                            //             recipe,
+                                            //           );
+                                            //         },
+                                            //       ),
+                                            //     ),
+                                            //   );
+                                            // }
+                                          } else {
+                                            favRecipeViewModel.addFavRecipe(
+                                              recipe.id,
+                                            );
+
+                                            if (toggleFavErrorMessage != '') {
+                                              ScaffoldMessenger.of(
+                                                context,
+                                              ).clearSnackBars();
+                                              ScaffoldMessenger.of(
+                                                context,
+                                              ).showSnackBar(
+                                                SnackBar(
+                                                  content: Text(
+                                                    toggleFavErrorMessage!,
+                                                  ),
+                                                  duration: Duration(
+                                                    seconds: 6,
+                                                  ),
+                                                  // action: SnackBarAction(
+                                                  //   label: 'DESFAZER',
+                                                  //   onPressed: () {
+                                                  //     viewModel.addToFavorites(
+                                                  //       recipe,
+                                                  //     );
+                                                  //   },
+                                                  // ),
+                                                ),
+                                              );
+                                            }
+
+                                            // if (context.mounted) {
+                                            //   ScaffoldMessenger.of(
+                                            //     context,
+                                            //   ).clearSnackBars();
+                                            //   ScaffoldMessenger.of(
+                                            //     context,
+                                            //   ).showSnackBar(
+                                            //     SnackBar(
+                                            //       content: Text(
+                                            //         '${recipe.name} favoritada!',
+                                            //       ),
+                                            //       duration: const Duration(
+                                            //         seconds: 2,
+                                            //       ),
+                                            //     ),
+                                            //   );
+                                            // }
+                                          }
+                                        },
+                                      ),
+                                    ),
                                   ],
                                 );
                               },
@@ -164,8 +227,7 @@ class _RecipesViewState extends State<RecipesView> {
                         ],
                       ),
                     )
-                  : 
-                  Center(
+                  : Center(
                       child: Column(
                         children: [
                           const SizedBox(height: 64),

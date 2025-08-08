9f14a3a691a2a227c3f2efd43c3e56dd682cff24
Welington Fidelis <welingtonfidelis@gmail.com>
2025-08-08 10:35:04 -0300

    Show favorite icon and remove recipe action included on favorite recipes page;

--- lib/ui/fav_recipes/fav_recipes_view.dart
+++ lib/ui/fav_recipes/fav_recipes_view.dart
@@ -81,9 +81,7 @@ class _RecipesViewState extends State<FavRecipesView> {
                               itemCount: viewModel.favRecipes.length,
                               itemBuilder: (context, index) {
                                 final recipe = viewModel.favRecipes[index];
-                                // final isFavorite = viewModel.favRecipes.any(
-                                //   (fav) => fav.id == recipe.id,
-                                // );
+
                                 return Stack(
                                   children: [
                                     GestureDetector(
@@ -91,70 +89,43 @@ class _RecipesViewState extends State<FavRecipesView> {
                                           context.go('/recipe/${recipe.id}'),
                                       child: RecipeCard(recipe: recipe),
                                     ),
-                                    // Positioned(
-                                    //   top: 16,
-                                    //   right: 16,
-                                    //   child: IconButton(
-                                    //     icon: Icon(
-                                    //       isFavorite
-                                    //           ? Icons.favorite
-                                    //           : Icons.favorite_border,
-                                    //       size: 32,
-                                    //       color: isFavorite ? Colors.red : null,
-                                    //     ),
-                                    //     onPressed: () {
-                                    //       if (isFavorite) {
-                                    //         viewModel.removeFromFavorites(
-                                    //           recipe,
-                                    //         );
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} desfavoritada!',
-                                    //               ),
-                                    //               duration: Duration(
-                                    //                 seconds: 3,
-                                    //               ),
-                                    //               action: SnackBarAction(
-                                    //                 label: 'DESFAZER',
-                                    //                 onPressed: () {
-                                    //                   viewModel.addToFavorites(
-                                    //                     recipe,
-                                    //                   );
-                                    //                 },
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       } else {
-                                    //         viewModel.addToFavorites(recipe);
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} favoritada!',
-                                    //               ),
-                                    //               duration: const Duration(
-                                    //                 seconds: 2,
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       }
-                                    //     },
-                                    //   ),
-                                    // ),
+                                    Positioned(
+                                      top: 16,
+                                      right: 16,
+                                      child: IconButton(
+                                        icon: Icon(
+                                          Icons.favorite,
+                                          size: 32,
+                                          color: Colors.red,
+                                        ),
+                                        onPressed: () {
+                                          viewModel.removeFavRecipe(recipe.id);
+                                          if (context.mounted) {
+                                            ScaffoldMessenger.of(
+                                              context,
+                                            ).clearSnackBars();
+                                            ScaffoldMessenger.of(
+                                              context,
+                                            ).showSnackBar(
+                                              SnackBar(
+                                                content: Text(
+                                                  '${recipe.name} desfavoritada!',
+                                                ),
+                                                duration: Duration(seconds: 3),
+                                                // action: SnackBarAction(
+                                                //   label: 'DESFAZER',
+                                                //   onPressed: () {
+                                                //     viewModel.addToFavorites(
+                                                //       recipe,
+                                                //     );
+                                                //   },
+                                                // ),
+                                              ),
+                                            );
+                                          }
+                                        },
+                                      ),
+                                    ),
                                   ],
                                 );
                               },
